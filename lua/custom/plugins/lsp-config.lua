00d0469e9b75097b2509ae417909114f05643b45
koushikyemula <koushikyemula@gmail.com>
2024-08-12 22:35:48 +0530

    config revert

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -1,159 +1,138 @@
 return {
-  {
-
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+  'neovim/nvim-lspconfig',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = {
+    { 'williamboman/mason.nvim', config = true },
+    'williamboman/mason-lspconfig.nvim',
+    { 'j-hui/fidget.nvim', opts = {} },
+    { 'b0o/schemastore.nvim' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+  },
+  config = function()
+    require('mason').setup {
+      ui = {
+        border = 'rounded',
+        icons = {
+          package_installed = '✓',
+          package_pending = '➜',
+          package_uninstalled = '✗',
+        },
       },
-    },
-  },
-  { 'Bilal2453/luvit-meta', lazy = true },
-  {
+    }
+    require('mason-lspconfig').setup {
+      ensure_installed = vim.tbl_keys(require 'custom.plugins.lsp.servers'),
+    }
+    require('lspconfig.ui.windows').default_options.border = 'single'
 
-    'neovim/nvim-lspconfig',
-    dependencies = {
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc)
+          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
 
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+        map('gd', require('telescope.builtin').lsp_definitions, 'Goto Definition')
+        map('gr', require('telescope.builtin').lsp_references, 'Goto References')
+        map('gi', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
+        map('go', require('telescope.builtin').lsp_type_definitions, 'Type Definition')
+        map('<leader>p', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
+        map('<leader>ws', require('telescope.builtin').lsp_workspace_symbols, 'Workspace Symbols')
+        map('<leader>Ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace Symbols')
 
-      { 'j-hui/fidget.nvim', opts = {} },
+        map('gl', vim.diagnostic.open_float, 'Open Diagnostic Float')
+        map('K', function()
+          vim.lsp.buf.hover()
+          -- Prevent entering the hover window
+          vim.cmd [[stopinsert]]
+        end, 'Hover Documentation')
+        map('gs', vim.lsp.buf.signature_help, 'Signature Documentation')
+        map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
-      'hrsh7th/cmp-nvim-lsp',
-    },
-    config = function()
-      -- Add TypeScript organize imports function
-      local function organize_imports()
-        local params = {
-          command = '_typescript.organizeImports',
-          arguments = { vim.api.nvim_buf_get_name(0) },
-          title = '',
-        }
-        vim.lsp.buf.execute_command(params)
-      end
+        map('<leader>v', '<cmd>vsplit | lua vim.lsp.buf.definition()<cr>', 'Goto Definition in Vertical Split')
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
+        local wk = require 'which-key'
+        wk.add {
+          { '<leader>ca', vim.lsp.buf.code_action, desc = 'Code Action' },
+          { '<leader>cA', vim.lsp.buf.range_code_action, desc = 'Range Code Actions' },
+          { '<leader>cs', vim.lsp.buf.signature_help, desc = 'Display Signature Information' },
+          { '<leader>cr', vim.lsp.buf.rename, desc = 'Rename all references' },
+          { '<leader>cf', vim.lsp.buf.format, desc = 'Format' },
+          { '<leader>ci', require('telescope.builtin').lsp_implementations, desc = 'Implementation' },
 
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          -- W = {
+          --   name = "+Workspace",
+          --   a = { vim.lsp.buf.add_workspace_folder, "Add Folder" },
+          --   r = { vim.lsp.buf.remove_workspace_folder, "Remove Folder" },
+          --   l = {
+          --     function()
+          --       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          --     end,
+          --     "List Folders",
+          --   },
+          -- },
 
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          -- Organize imports keybind
-          map('<leader>O', organize_imports, '[R]efactor: Organize [I]mports')
-
-          -- Document highlight disabled
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
-        end,
-      })
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-      -- Add TypeScript organize imports function
-      local function organize_imports()
-        local params = {
-          command = '_typescript.organizeImports',
-          arguments = { vim.api.nvim_buf_get_name(0) },
-          ts_ls = {
-            commands = {
-              OrganizeImports = {
-                organize_imports,
-                description = 'Organize Imports',
-              },
-            },
+          { '<leader>Wa', vim.lsp.buf.add_workspace_folder, desc = 'Workspace Add Folder' },
+          { '<leader>Wr', vim.lsp.buf.remove_workspace_folder, desc = 'Workspace Remove Folder' },
+          {
+            '<leader>Wl',
+            function()
+              print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end,
+            desc = 'Workspace List Folders',
           },
-          vim.lsp.buf.execute_command(params),
         }
-      end
 
-      local servers = {
+        -- Document highlight removed
+      end,
+    })
 
-        gopls = {},
-        pyright = {},
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-        ts_ls = {},
+    local mason_lspconfig = require 'mason-lspconfig'
 
-        lua_ls = {
+    mason_lspconfig.setup_handlers {
+      function(server_name)
+        require('lspconfig')[server_name].setup {
+          capabilities = capabilities,
+          -- on_attach = require("plugins.lsp.on_attach").on_attach,
+          settings = require('custom.plugins.lsp.servers')[server_name],
+          filetypes = (require('custom.plugins.lsp.servers')[server_name] or {}).filetypes,
+        }
+      end,
+    }
 
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-            },
-          },
-        },
-      }
+    -- Gleam LSP
+    -- For some reason mason doesn't work with gleam lsp
+    require('lspconfig').gleam.setup {
+      cmd = { 'gleam', 'lsp' },
+      filetypes = { 'gleam' },
+      root_dir = require('lspconfig').util.root_pattern('gleam.toml', '.git'),
+      capabilities = capabilities,
+    }
 
-      require('mason').setup()
+    vim.diagnostic.config {
+      title = false,
+      underline = false,
+      virtual_text = true,
+      signs = true,
+      update_in_insert = false,
+      severity_sort = true,
+      float = {
+        source = 'if_many',
+        style = 'minimal',
+        border = 'rounded',
+        header = '',
+        prefix = '',
+      },
+    }
 
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua',
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-      vim.diagnostic.config {
-        title = false,
-        underline = false,
-        virtual_text = true,
-        signs = true,
-        update_in_insert = false,
-        severity_sort = true,
-        float = {
-          source = 'if_many',
-          style = 'minimal',
-          border = 'rounded',
-          header = '',
-          prefix = '',
-        },
-      }
-
-      local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
-      for type, icon in pairs(signs) do
-        local hl = 'DiagnosticSign' .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-      end
-    end,
-  },
+    local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
+    for type, icon in pairs(signs) do
+      local hl = 'DiagnosticSign' .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+    end
+  end,
 }
