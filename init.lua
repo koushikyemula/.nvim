371e3371777523e6cc7afffe4df6dac2ffa8c0f4
koushikyemula <koushikyemula@gmail.com>
2024-11-15 22:59:22 +0530

    update theme

--- init.lua
+++ init.lua
@@ -753,21 +753,20 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'rebelot/kanagawa.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      vim.cmd.colorscheme 'kanagawa'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+    config = function()
+      require('kanagawa').setup {
+        transparent = true,
+        colors = {
+          palette = {
+            sumiInk1 = '#000000',
+          },
+        },
+      }
+      vim.cmd 'colorscheme kanagawa'
     end,
+    priority = 1000,
   },
 
   -- Highlight todo, notes, etc in comments
