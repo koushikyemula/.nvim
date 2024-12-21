81448e70cf682308d420b94fc8aa5846f98f99fd
koushikyemula <koushikyemula@gmail.com>
2024-12-21 19:49:59 +0530

    new colorscheme and oil nvmim

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,10 +2,17 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    -- Don't automatically disable Netrw
+    default_file_explorer = false,
+  },
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      default_file_explorer = false,
+    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    -- Keep your existing Netrw binding
+    vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open Netrw' })
   end,
 }
