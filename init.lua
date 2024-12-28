bc68ca29de1f2a17c50aac8e4a858070ad434ee5
koushikyemula <koushikyemula@gmail.com>
2024-12-28 23:49:39 +0530

    colorscheme changes

--- init.lua
+++ init.lua
@@ -341,13 +341,13 @@ require('lazy').setup({
 
       require('mini.surround').setup()
 
-      local statusline = require 'mini.statusline'
+      -- local statusline = require 'mini.statusline'
 
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- statusline.section_location = function()
+      --   return '%2l:%-2v'
+      -- end
     end,
   },
   {
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -13,30 +13,29 @@ return {
           theme = {
             all = {
               ui = {
-                bg = '#080808',        -- Very dark background like lackluster
-                bg_gutter = '#080808', -- Dark background for line numbers
+                bg = '#080808',
+                bg_gutter = '#080808',
               },
             },
           },
         },
         overrides = function()
           return {
-            LineNr = { bg = '#080808' },                                    -- Line numbers
-            SignColumn = { bg = '#080808' },                                -- Sign column
-            Normal = { bg = '#080808' },                                    -- Normal text background
-            NormalFloat = { bg = '#080808' },                               -- Float windows
+            LineNr = { bg = '#080808' },
+            SignColumn = { bg = '#080808' },
+            Normal = { bg = '#080808' },
+            NormalFloat = { bg = '#080808' },
             FloatBorder = { bg = '#080808' },
-            CursorLine = { bg = '#151515' },                                -- Current line highlight
-            CursorLineNr = { bg = '#080808', fg = '#626262', bold = true }, -- Current line number
+            CursorLine = { bg = '#151515' },
+            CursorLineNr = { bg = '#080808', fg = '#626262', bold = true },
 
-            -- TreeSitter Context
             TreesitterContext = { bg = '#080808' },
             TreesitterContextLineNumber = { bg = '#080808' },
             TreesitterContextBottom = { bg = '#080808' },
           }
         end,
       }
-      -- Apply the colorscheme
+
       vim.cmd.colorscheme 'kanagawa'
     end,
   },
