c296d880a66ae8f25654364653a4108c857352e5
koushikyemula <koushikyemula@gmail.com>
2025-01-02 00:59:38 +0530

    init lua changes

--- init.lua
+++ init.lua
@@ -258,6 +258,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sc', builtin.colorscheme, { desc = 'Search Colorscheme' })
 
       vim.keymap.set('n', '<leader>/', function()
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
