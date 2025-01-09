5bbf19b70f63cb81822ac6befdd5821865573a12
koushikyemula <koushikyemula@gmail.com>
2025-01-09 23:47:38 +0530

    blitz fugitive blitz fugitive

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -3,11 +3,11 @@ return {
   config = function()
     vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 
-    local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup('ThePrimeagen_Fugitive', {})
+    local BlitZ_Fugitive = vim.api.nvim_create_augroup('BlitZ_Fugitive', {})
 
     local autocmd = vim.api.nvim_create_autocmd
     autocmd('BufWinEnter', {
-      group = ThePrimeagen_Fugitive,
+      group = BlitZ_Fugitive,
       pattern = '*',
       callback = function()
         if vim.bo.ft ~= 'fugitive' then
