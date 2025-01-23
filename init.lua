4d57d74d0897ce9626962026fbfe8a40b0cf6f8f
Koushik <koushikyemula@gmail.com>
2025-01-23 21:25:31 +0530

    Show hidden files in telescope and oil nvim

--- init.lua
+++ init.lua
@@ -233,10 +233,22 @@ require('lazy').setup({
               ['<C-j>'] = require('telescope.actions').move_selection_next,
             },
           },
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+            '--glob=!.git/*',
+          },
         },
         pickers = {
           find_files = {
             hidden = true,
+            file_ignore_patterns = { '^.git/' },
           },
         },
         extensions = {
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -5,14 +5,13 @@ return {
   opts = {
     -- Don't automatically disable Netrw
     default_file_explorer = false,
+    view_options = {
+      show_hidden = true,
+    },
   },
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
-  config = function()
-    require('oil').setup {
-      default_file_explorer = false,
-    }
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-    -- Keep your existing Netrw binding
-    vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open Netrw' })
-  end,
+  keys = {
+    { '-',          '<CMD>Oil<CR>', desc = 'Open parent directory' },
+    { '<leader>pv', vim.cmd.Ex,     desc = 'Open Netrw' },
+  },
 }
