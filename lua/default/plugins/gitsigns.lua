e1cb2b7c11b6484a7b9f6817a69318eac95404ae
Koushik <koushikyemula@gmail.com>
2025-01-25 22:53:12 +0530

    diff view and keybinds

--- lua/default/plugins/gitsigns.lua
+++ lua/default/plugins/gitsigns.lua
@@ -58,4 +58,22 @@ return {
       end,
     },
   },
+  {
+    'sindrets/diffview.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose' },
+    keys = {
+      { '<leader>dv', '<cmd>DiffviewFileHistory %<cr>', desc = 'View git history for current file' },
+      { '<leader>dc', '<cmd>DiffviewClose<cr>', desc = 'Close Diffview' },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      use_icons = true,
+      view = {
+        default = {
+          layout = 'diff2_horizontal',
+        },
+      },
+    },
+  },
 }
