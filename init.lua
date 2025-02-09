72823c6c49f6edb408c56b312903e9f086d6af8c
Koushik <koushikyemula@gmail.com>
2025-02-09 18:55:13 +0530

    add proper comment plugin and dep

--- init.lua
+++ init.lua
@@ -271,6 +271,32 @@ require('lazy').setup({
     end,
   },
   {
+    'numToStr/Comment.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    config = function()
+      require('Comment').setup {
+        pre_hook = function(ctx)
+          -- Check if we're in a JSX/TSX file
+          local U = require 'Comment.utils'
+          local location = nil
+          if ctx.ctype == U.ctype.blockwise then
+            location = require('ts_context_commentstring.utils').get_cursor_location()
+          elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+            location = require('ts_context_commentstring.utils').get_visual_start_location()
+          end
+
+          return require('ts_context_commentstring.internal').calculate_commentstring {
+            key = ctx.ctype == U.ctype.linewise and '__default' or '__multiline',
+            location = location,
+          }
+        end,
+      }
+    end,
+  },
+  {
     'yetone/avante.nvim',
     event = 'VeryLazy',
     lazy = false,
