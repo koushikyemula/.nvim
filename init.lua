b1aec33880da85bc24647e5d87aeebb784b7589e
Koushik <koushikyemula@gmail.com>
2025-02-20 19:17:56 +0530

    chore: horizontal scrolling binds

--- init.lua
+++ init.lua
@@ -52,6 +52,8 @@ vim.diagnostic.config { virtual_lines = true }
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'zh', '4zh')
+vim.keymap.set('n', 'zl', '4zl')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
