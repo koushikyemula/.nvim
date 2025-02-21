24128c046daa4c991ab3e24a2023d44dc618c4a8
Koushik <koushikyemula@gmail.com>
2025-02-21 22:54:59 +0530

    better status line colors

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -29,9 +29,9 @@ return {
       desc = 'Zen mode',
     },
     {
-      '<leader>ti', 
+      '<leader>ti',
       function()
-        Snacks.image.hover() 
+        Snacks.image.hover()
       end,
       desc = 'Toggle image preview',
     },
--- lua/custom/plugins/statusline.lua
+++ lua/custom/plugins/statusline.lua
@@ -4,30 +4,30 @@ return {
     options = {
       theme = {
         normal = {
-          --          a = { bg = '#FF5555', fg = '#282A36' },
-          a = { bg = '#662222', fg = '#BBBBBB' }, -- Even darker red-brown
+          a = { bg = '#BD93F9', fg = '#282A36' },
+          -- a = { bg = '#662222', fg = '#BBBBBB' }, -- Even darker red-brown
           b = { bg = '#2B2B2B', fg = '#F8F8F2' },
           c = { bg = '#303030', fg = '#F8F8F2' },
         },
         insert = {
-          --a = { bg = '#61AFEF', fg = '#282A36' },
-          a = { bg = '#8B4513', fg = '#BBBBBB' }, -- Darker saddle brown orange
+          a = { bg = '#FFB86C', fg = '#282A36' },
+          -- a = { bg = '#8B4513', fg = '#BBBBBB' }, -- Darker saddle brown orange
           b = { bg = '#2B2B2B', fg = '#F8F8F2' },
           c = { bg = '#303030', fg = '#F8F8F2' },
         },
         visual = {
-          a = { bg = '#FFB86C', fg = '#282A36' },
+          a = { bg = '#61AFEF', fg = '#282A36' },
           b = { bg = '#2B2B2B', fg = '#F8F8F2' },
           c = { bg = '#303030', fg = '#F8F8F2' },
         },
         replace = {
-          a = { bg = '#BD93F9', fg = '#282A36' },
+          a = { bg = '#FF5555', fg = '#282A36' },
           b = { bg = '#2B2B2B', fg = '#F8F8F2' },
           c = { bg = '#303030', fg = '#F8F8F2' },
         },
         command = {
-          --a = { bg = '#FF79C6', fg = '#282A36' },
-          a = { bg = '#0F4214', fg = '#BBBBBB' }, -- Much darker forest green
+          a = { bg = '#FF79C6', fg = '#282A36' },
+          -- a = { bg = '#0F4214', fg = '#BBBBBB' }, -- Much darker forest green
           b = { bg = '#2B2B2B', fg = '#F8F8F2' },
           c = { bg = '#303030', fg = '#F8F8F2' },
         },
