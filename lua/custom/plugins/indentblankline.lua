8932de7ecd8634b78131305b2a18a5d6efa966ba
Koushik <koushikyemula@gmail.com>
2025-03-04 22:12:35 +0530

    finally got the perfect indent line colors

--- lua/custom/plugins/indentblankline.lua
+++ lua/custom/plugins/indentblankline.lua
@@ -1,20 +1,23 @@
 return {
-  { -- Add indentation guides even on blank lines
+  {
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
     opts = {
       indent = {
-        -- Set a very dim color for normal indent lines
-        highlight = { 'NonText' },
+        highlight = 'IndentLine',
         char = '│',
       },
       scope = {
-        enabled = true, -- enable scope highlighting
-        show_start = false, -- don't show the start of the scope
-        show_end = false, -- don't show the end of the scope
+        enabled = true,
+        highlight = 'IndentLineActive',
+        show_start = false,
+        show_end = false,
       },
     },
+    config = function(_, opts)
+      vim.api.nvim_set_hl(0, 'IndentLine', { fg = '#2C2C2C' })
+      vim.api.nvim_set_hl(0, 'IndentLineActive', { fg = '#707070' })
+      require('ibl').setup(opts)
+    end,
   },
 }
