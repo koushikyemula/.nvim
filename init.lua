79105b6ec911b7cd8201aae6caa2f7081b2c64d5
Koushik <koushikyemula@gmail.com>
2025-03-10 22:34:17 +0530

    more useful vim opts

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-
+vim.opt.swapfile = false
 vim.opt.wrap = false
 
 vim.opt.mouse = 'a'
@@ -33,6 +33,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
 
 vim.opt.updatetime = 250
 
--- lua/default/plugins/autopairs.lua
+++ lua/default/plugins/autopairs.lua
@@ -1,5 +1,5 @@
 return {
-  'windwp/nvim-autopairs',
+  --[[ 'windwp/nvim-autopairs',
   event = 'InsertEnter',
 
   dependencies = { 'hrsh7th/nvim-cmp' },
@@ -15,5 +15,5 @@ return {
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-  end,
+  end, ]]
 }
