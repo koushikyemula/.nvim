ed20c0e080ef216fff7c682a444541c07bdecefe
Koushik <koushikyemula@gmail.com>
2025-03-16 15:01:51 +0530

    transparent bg varient

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -10,34 +10,45 @@ return {
           dark = 'wave',
           light = 'lotus',
         },
+        transparent = true, -- Enable transparency
         colors = {
           theme = {
             all = {
               ui = {
-                bg = '#080808',
-                bg_gutter = '#080808',
+                -- bg = '#080808', -- Commented out for transparency
+                -- bg_gutter = '#080808', -- Commented out for transparency
               },
             },
           },
         },
         overrides = function()
           return {
-            SignColumn = { bg = '#080808' },
-            Normal = { bg = '#080808' },
-            NormalFloat = { bg = '#080808' },
-            FloatBorder = { bg = '#080808' },
+            -- SignColumn = { bg = '#080808' },
+            Normal = { bg = 'NONE' }, -- Use NONE for transparency
+            -- NormalFloat = { bg = '#080808' },
+            -- FloatBorder = { bg = '#080808' },
             CursorLine = { bg = '#151515' },
 
-            LineNr = { bg = '#080808', fg = '#515151' },
-            CursorLineNr = { bg = '#080808', fg = '#B0B0B0' },
+            -- LineNr = { bg = '#080808', fg = '#515151' },
+            LineNr = { bg = 'NONE', fg = '#515151' },
+            -- CursorLineNr = { bg = '#080808', fg = '#B0B0B0' },
+            CursorLineNr = { bg = 'NONE', fg = '#B0B0B0' },
 
-            TreesitterContext = { bg = '#080808' },
-            TreesitterContextLineNumber = { bg = '#080808' },
-            TreesitterContextBottom = { bg = '#080808' },
+            -- TreesitterContext = { bg = '#080808' },
+            -- TreesitterContextLineNumber = { bg = '#080808' },
+            -- TreesitterContextBottom = { bg = '#080808' },
+            TreesitterContext = { bg = 'NONE' },
+            TreesitterContextLineNumber = { bg = 'NONE' },
+            TreesitterContextBottom = { bg = 'NONE' },
           }
         end,
       }
       vim.cmd.colorscheme 'kanagawa'
+
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
+      vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE' })
+      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE' })
     end,
   },
   {
