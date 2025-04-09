29afb041a9cb575c7ad3177a32a64a9d0492b8d2
Koushik <koushikyemula@gmail.com>
2025-04-09 11:34:14 +0530

    new theme and scroll animation

--- init.lua
+++ init.lua
@@ -202,7 +202,14 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup {
+        duration_multiplier = 0, -- Default is 1.0, smaller means faster animations
+      }
+    end,
+  },
   {
     'numToStr/Comment.nvim',
     event = 'VeryLazy',
